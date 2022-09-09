a88366f4bc4edc35d8232fc5a4e1dd0a2a798061
Cricboi911 <shereboi911@gmail.com>
2022-09-09 02:13:57 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - the program starts and ends with the main
  * Return: the main function returns zero when success
  */
-int main(void)  
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");  
-return (0);  
+puts("\"Programming is like building a multilingual puzzle"); 
+return (0);
 }
