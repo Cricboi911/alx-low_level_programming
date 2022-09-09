5876baaae5a3293c3ae486b2c8dd711ae698145f
Cricboi911 <shereboi911@gmail.com>
2022-09-09 02:20:44 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - the function starts and ends with the main function
  * Return: the main returns 0 upon completion and success
  */
-int main(void)  
+int main(void) 
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");  
-return (0); 
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
