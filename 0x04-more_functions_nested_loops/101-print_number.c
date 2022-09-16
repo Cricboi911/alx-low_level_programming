8c9a905cb8cfc9a62515efbccbe7dcb7323d0496
cricboi911 <shereboi911@gmail.com>
2022-09-16 00:16:49 -0700

    new

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -19,15 +19,15 @@ void print_number(int n)
 	{
 		i = n;
 	}
+
 	j = i;
-	count = 1;
 
 	while (j > 9)
 	{
 		j /= 10;
 		count *= 10;
 	}
-	for (; count >= 1; count / 10)
+	for (count = 1; count >= 1; (count /= 10))
 	{
 		_putchar(((i / count) % 10) + 48);
 	}
